89c537ea14a859a521972296dec2479c0e327715
SapphireGreen <prashantmani334@gmail.com>
2023-12-16 11:03:51 +0530

    output file name automated with log date

--- logSearch.py
+++ logSearch.py
@@ -2,14 +2,15 @@
 import os
 import re
 
-file_name = "kazam-qrh55z.txt"
+file_name = "kazam-ww-su-log-04-0-01-24-11-2023.txt"
 
-device_id = "do0142"
+device_id = "u9oi9b"
+log_date = "_23-11-2023"
 
-path = rf"C:\Users\HP\Documents\dev\Logs\{device_id}"
+path = rf"C:\Users\HP\Documents\dev\Logs\{device_id + log_date}"
 
 output_filename = os.path.normpath(path)
-regex = r"{\s*device_id:\s*'do0142'[\s\S]*?}(?=\n\n|$)"
+regex = r"{\s*device_id:\s*'u9oi9b'[\s\S]*?}(?=\n\n|$)"
 
 
 with open(file_name, 'r', encoding="utf8") as input:
